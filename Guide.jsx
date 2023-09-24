5af9cb8602f0944f116349312af1284b05940542
priyank071 <139440914+priyank071@users.noreply.github.com>
2023-09-24 18:25:09 +0530

    Guide

---
+++ Guide.jsx
@@ -0,0 +1,62 @@
+const Guide=()=>{
+    return(
+        <>
+        <div className="guide-main">
+        <div className="guide-content">
+         <h2>How to Dispose of E-Waste Correctly:
+        </h2>
+        <hr/>
+        <ul>
+            <li>
+                <h3 className="sub-topic-guide">Find a Recycling Center:</h3 >
+                <p> Locate a nearby e-waste recycling center or drop-off point in your area.
+               </p>
+            </li>
+            <li>
+                <h3 className="sub-topic-guide"> Prepare Your Devices:</h3>
+                <p>Remove personal data and information from your devices. Back up important files and perform a factory reset or data wipe.
+                </p>
+            </li>
+            <li>
+                <h3 className="sub-topic-guide">Pack Carefully:</h3 >
+                <p>Use suitable packaging like cardboard boxes or containers for safe transportation.
+                </p>
+            </li>
+            <li>
+                <h3 className="sub-topic-guide">Dispose Responsibly: </h3>
+                <p> Follow the instructions at the recycling center or collection event to ensure proper disposal.
+                </p>
+            </li>
+        </ul>
+        <h2>What NOT to Do:
+        </h2>
+        <ul>
+            <li>
+                <h3 className="sub-topic-guide">Don't Throw in Trash: </h3 >
+                <p>Never toss electronic devices in regular trash bins or dumpsters.
+                </p>
+            </li>
+            <li>
+                <h3 className="sub-topic-guide">Avoid Landfills:</h3 >
+                <p> E-waste should not end up in landfills where it can leach harmful chemicals into the environment.
+                </p> 
+            </li>
+            <li>
+                <h3 className="sub-topic-guide">No Illegal Dumping:</h3 >
+                <p>Illegally dumping e-waste on streets or vacant lots is harmful and illegal.
+                </p>
+            </li>
+            <li>
+                <h3 className="sub-topic-guide">Beware of Scams: </h3>
+                <p>Be cautious of unverified e-waste disposal services that may mishandle or export e-waste illegally.
+                Remember, responsible disposal of e-waste protects the environment and conserves resources.</p>
+            </li>
+        </ul>
+        <div><pre> "Let's all do our part to keep our communities clean and sustainable."</pre>
+        </div>
+        </div>
+        </div>
+        </>
+    );
+}
+export default Guide;
