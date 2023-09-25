afff522ef5b9081971ec6d9de69f4f741d5dcff2
priyank071 <139440914+priyank071@users.noreply.github.com>
2023-09-25 09:41:07 +0530

    Update HomePage.jsx

--- HomePage.jsx
+++ HomePage.jsx
@@ -3,7 +3,8 @@ import img2 from './plant.png';
 const HomePage=()=>{
     return(
         <>
-        <div className="home-page">
+        <div className="home-page landing-page">
+            <div className='extra'></div>
             <div className='nav-my'>
             <img className='logo' src={img1}></img>
             <div className='con-home'>
@@ -11,6 +12,11 @@ const HomePage=()=>{
             <div className="font  con-home contact"><a href="">Contact</a></div>
             <div className=" font con-home faqs"><a href="">FAQs</a></div>
             </div>
+            <div class="blob-circle blob-my">
+           <span></span>
+           <span></span>
+           <span></span>
+           </div>    
             <div className='body-my'>
             <div className='leaf-div'><img className='leaf' src={img2}></img></div>
             <h1 className='font title'>Welcome To Green<h1 id='vision'>Vision</h1></h1>
@@ -21,9 +27,9 @@ const HomePage=()=>{
             </div>
             </div>
         </div>
-        <div className='ele'>
+        {/* <div className='ele'>
         <div className='element element-1'></div>
-        </div>
+        </div> */}
         </>
     );
 }
